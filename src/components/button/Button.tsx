8587b551003b5ad5493ab039d2592f8fab295e4b
r4lfl4b <dev@rgalan.es>
2019-10-21 13:11:31 +0100

    add button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Button.scss';
+
+interface IProps {
+  /**
+   * Specifies button click event
+   */
+  onClick: () => void;
+
+  className?: string;
+
+  /**
+   * Specifies the aria-label text
+   *
+   */
+  ariaLabel: string;
+
+}
+
+const Button:React.FunctionComponent<IProps> = props => {
+  const {
+    children,
+    onClick = () => {},
+    className: cssClasses = '',
+    ariaLabel = ""
+  } = props;
+
+
+  const handleClick = () => {
+    onClick();
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={cssClasses}
+      aria-label={ariaLabel}
+    >
+      {children && (
+        <span className="cs-button__text">
+          {children}
+        </span>
+      )}
+    </button>
+  );
+};
+
+export default Button;

c36d9cce316989e541e58c6735ed9be1ea2f08f5
r4lfl4b <dev@rgalan.es>
2019-10-21 11:17:31 +0100

    style: center app and set max-width

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import './App.scss';
 import Header from './components/header/Header';
 
-
 function App() {
   return (
-    <div className="cs">
+    <div className="cs-app">
       <Header heading="Sporting Quotes"/>
     </div>
   );
